d9bc42b04af8a92518263b64276dfaeabd15dc8d
TuanTu1007 <21522747@gm.uit.edu.vn>
2024-12-12 16:30:08 +0700

    BE: CREATE categoriesEntity

---
+++ src/main/java/Entity/categoriesEntity.java
@@ -0,0 +1,27 @@
+package Entity;
+
+public class categoriesEntity {
+	private int categoryId;
+    private String categoryName;
+
+    public categoriesEntity(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
